175254c0d829e458f86ff3e0297b0cf19b19fb9b
manipala <manipala@CHNSIPDT0T816>
2018-07-18 14:20:50 +0530

    Add project files.

---
+++ WebApplication1/DisplayResults.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class DisplayResults : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["userId"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            Master.Logout = true;
+            Master.Login = false;
+            Master.Signup = false;
+            Master.Profile = true;
+            Master.lbl_Profile = Session["userName"].ToString();
+        }
+    }
+}
